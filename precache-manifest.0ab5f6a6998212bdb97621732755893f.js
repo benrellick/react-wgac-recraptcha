7516e00f6c53f653d37b012a30400f8f3b0c9a25
Ben Rellick <benrellick@gmail.com>
2021-06-11 19:03:05 -0400

    Updates

---
+++ precache-manifest.0ab5f6a6998212bdb97621732755893f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8b2545e2e37c4966763cbdaf9ecac3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d39e2b23ea5e1a9c6a93",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "c5ede77c2d24d6d036ce",
+    "url": "./static/js/2.bb1c91bf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bb1c91bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d39e2b23ea5e1a9c6a93",
+    "url": "./static/js/main.c847df79.chunk.js"
+  },
+  {
+    "revision": "676134159ba1236d8ba3",
+    "url": "./static/js/runtime-main.84d20d53.js"
+  }
+]);
